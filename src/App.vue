0faa69b187773deabf3723d0a82e181485d043f1
suhan <completed0728@gmail.com>
2021-10-12 18:01:49 +0900

    beforeCreate, created, beforeMount, mounted 라이프사이클 훅

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <!-- Remove & use example component! -->
-  <HelloWorld 
-    :msg="message" 
-    @click="handler" />
-  <HelloWorld msg="Good" />
+  <h1>{{ count }}</h1>
 </template>
 
 <script>
-import HelloWorld from '~/components/HelloWorld'
-
 export default {
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      message: 'Hello Vue!'
+  data(){
+    return{
+      count:2
     }
   },
-  methods: {
-    handler() {
-      console.log(this.message)
-      this.message += '!'
-    }
+  beforeCreate(){
+    console.log(`beforeCreate, (데이터/돔에 접근 불가) count : ${this.count}`);
+  },
+  created(){
+    console.log(`created : 인스턴스가 생성된 후에 코드를 실행하는데 사용됩니다, (데이터만 접근가능) count : ${this.count}`);
+  },
+  beforeMount(){
+    console.log(`beforeMount 돔에 접근 불가능, h1 : ${document.querySelector('h1')}`);
+  },
+  mounted(){
+    console.log(`mounted : 화면에 렌더링 될때 실행 / 돔에 접근가능, h1 : ${document.querySelector('h1')}`);
   }
 }
 </script>
+
+<style>
+
+</style>
